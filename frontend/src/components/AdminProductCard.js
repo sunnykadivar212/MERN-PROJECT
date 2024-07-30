b7676c3100c2be6946043fd6276cd8bff18703e9
sunnykadivar212 <sunnykadivar0689@gmail.com>
2024-07-30 11:59:53 +0530

    Comlete E-commerce project

---
+++ frontend/src/components/AdminProductCard.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { MdOutlineModeEdit } from "react-icons/md";
+import AdminEditProduct from "./AdminEditProduct";
+import displayINRCurrency from "../helpers/displayCurrency";
+
+const AdminProductCard = ({ data, fetchdata }) => {
+  const [editProduct, setEditProduct] = useState(false);
+  return (
+    <div className="bg-white p-4 rounded">
+      <div className="w-40">
+        <div className="w-32 h-32 flex justify-center items-center">
+          <img
+            src={data?.productImage[0]}
+            className="mx-auto object-fill h-full"
+          />
+        </div>
+        <h1 className="text-ellipsis line-clamp-2">{data?.productName}</h1>
+
+        <div>
+          <p className="font-semibold">
+            {displayINRCurrency(data?.sellingPrice)}
+          </p>
+          <div
+            className="w-fit ml-auto p-2 bg-blue-200 rounded-full hover:bg-blue-500 hover:text-white cursor-pointer"
+            onClick={() => setEditProduct(true)}
+          >
+            <MdOutlineModeEdit />
+          </div>
+        </div>
+      </div>
+
+      {editProduct && (
+        <AdminEditProduct
+          productData={data}
+          onClose={() => setEditProduct(false)}
+          fetchdata={fetchdata}
+        />
+      )}
+    </div>
+  );
+};
+
+export default AdminProductCard;
